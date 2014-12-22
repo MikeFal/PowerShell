36586e64ff77e9a338054d1bc109d9feaf5cdaa7
dev-mikef <mike.fal@xero.com>
2014-12-22 15:43:45 -0700

    Added Expand-SqlLogFile and Get-FileVolume functions

---
+++ Get-FileVolume.ps1
@@ -0,0 +1,17 @@
+﻿function Get-FileVolume{
+    param([string]$PathName)
+
+    $check = $PathName.Substring(0,$PathName.LastIndexOf('\')+1)
+
+    $volumes = (gwmi win32_volume| where {$_.drivetype -eq 3}|select name|out-string -stream).Trim()
+    if($volumes -contains $check){
+        
+        return gwmi win32_volume| where {$_.name -eq $check}
+    }
+    else{
+        return Get-FileVolume $check.Substring(0,$check.Length-1)
+
+    }
+}
+
+Get-FileVolume 'U:\ag07\RaiseTicket\RaiseTicket.mdf'

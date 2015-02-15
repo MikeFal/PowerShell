cd642da990c27502e0474e85ea3dcab3eadb8c54
dev-mikef <mike.fal@xero.com>
2015-02-15 11:59:17 -0700

    Function to set default databases for all logins on a list of instances.

---
+++ Set-SQLDefaultDatabases.ps1
@@ -0,0 +1,23 @@
+﻿#load assemblies
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
+$ErrorActionPreference = 'Inquire'
+
+function Set-SQLDefaultDatabases{
+    param([string[]]$Instances = 'localhost'
+    ,[string]$defaultdb = 'tempdb')
+
+    foreach($InstanceName in $Instances){
+        $smosrv = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server $InstanceName
+        if($smosrv.Databases.name -contains $defaultdb){
+            foreach($login in $smosrv.Logins){
+                Write-Verbose "Altering $login on $InstanceName to $defaultdb"
+                $login.DefaultDatabase = $defaultdb
+            }
+        }
+        else{
+            Write-Warning "Database $defaultdb is not valid on $InstanceName."
+        }
+    }
+}
+
+Set-SQLDefaultDatabases -defaultdb tempdb -verbose

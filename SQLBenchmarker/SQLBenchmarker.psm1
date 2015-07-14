6f3426da30cc1911e53974d9c08d2afea0cf3542
Mike F <mike@mikefal.net>
2015-07-14 09:53:14 -0600

    Added Get-SQLWaitStats to SQLBenchnmarker

--- SQLBenchmarker/SQLBenchmarker.psm1
+++ SQLBenchmarker/SQLBenchmarker.psm1
@@ -181,3 +181,52 @@ function Get-SQLIO{
 		}
 }
 
+function Get-SQLWaitStats{
+param([string]$InstanceName='localhost'
+        ,[int]$DurationSec=5
+	)
+
+	$smo = new-object ('Microsoft.SqlServer.Management.Smo.Server')
+
+	$tlbname = $tblname = 'WS_'+(([char[]]([char]'a'..[char]'z') + 0..9 | sort {get-random})[0..5] -join '').ToUpper()
+	$sqlstart = "SELECT wait_type,waiting_tasks_count,wait_time_ms INTO $tblname FROM sys.dm_os_wait_stats;"
+	$sqlend = @"
+SELECT
+    e.wait_type
+    ,e.waiting_tasks_count - s.waiting_tasks_count waiting_tasks
+    ,e.wait_time_ms - s.wait_time_ms wait_time_ms
+FROM
+    sys.dm_os_wait_stats e
+    JOIN $tblname s ON e.wait_type = s.wait_type
+WHERE e.[wait_type] NOT IN (
+    N'BROKER_EVENTHANDLER', N'BROKER_RECEIVE_WAITFOR', N'BROKER_TASK_STOP',
+	N'BROKER_TO_FLUSH', N'BROKER_TRANSMITTER', N'CHECKPOINT_QUEUE',
+    N'CHKPT', N'CLR_AUTO_EVENT', N'CLR_MANUAL_EVENT', N'CLR_SEMAPHORE',
+    N'DBMIRROR_DBM_EVENT', N'DBMIRROR_EVENTS_QUEUE', N'DBMIRROR_WORKER_QUEUE',
+	N'DBMIRRORING_CMD', N'DIRTY_PAGE_POLL', N'DISPATCHER_QUEUE_SEMAPHORE',
+    N'EXECSYNC', N'FSAGENT', N'FT_IFTS_SCHEDULER_IDLE_WAIT', N'FT_IFTSHC_MUTEX',
+    N'HADR_CLUSAPI_CALL', N'HADR_FILESTREAM_IOMGR_IOCOMPLETION', N'HADR_LOGCAPTURE_WAIT', 
+	N'HADR_NOTIFICATION_DEQUEUE', N'HADR_TIMER_TASK', N'HADR_WORK_QUEUE',
+    N'KSOURCE_WAKEUP', N'LAZYWRITER_SLEEP', N'LOGMGR_QUEUE', N'ONDEMAND_TASK_QUEUE',
+    N'PWAIT_ALL_COMPONENTS_INITIALIZED', N'QDS_PERSIST_TASK_MAIN_LOOP_SLEEP',
+    N'QDS_CLEANUP_STALE_QUERIES_TASK_MAIN_LOOP_SLEEP', N'REQUEST_FOR_DEADLOCK_SEARCH',
+	N'RESOURCE_QUEUE', N'SERVER_IDLE_CHECK', N'SLEEP_BPOOL_FLUSH', N'SLEEP_DBSTARTUP',
+	N'SLEEP_DCOMSTARTUP', N'SLEEP_MASTERDBREADY', N'SLEEP_MASTERMDREADY',
+    N'SLEEP_MASTERUPGRADED', N'SLEEP_MSDBSTARTUP', N'SLEEP_SYSTEMTASK', N'SLEEP_TASK',
+    N'SLEEP_TEMPDBSTARTUP', N'SNI_HTTP_ACCEPT', N'SP_SERVER_DIAGNOSTICS_SLEEP',
+	N'SQLTRACE_BUFFER_FLUSH', N'SQLTRACE_INCREMENTAL_FLUSH_SLEEP', N'SQLTRACE_WAIT_ENTRIES',
+	N'WAIT_FOR_RESULTS', N'WAITFOR', N'WAITFOR_TASKSHUTDOWN', N'WAIT_XTP_HOST_WAIT',
+	N'WAIT_XTP_OFFLINE_CKPT_NEW_LOG', N'WAIT_XTP_CKPT_CLOSE', N'XE_DISPATCHER_JOIN',
+    N'XE_DISPATCHER_WAIT', N'XE_TIMER_EVENT')
+AND e.[waiting_tasks_count] > 0
+"@
+
+	$smo.Databases['tempdb'].ExecuteNonQuery($sqlstart)
+	Start-Sleep -Seconds $duration
+	$output = ($smo.Databases['tempdb'].ExecuteWithResults($sqlend)).Tables[0]
+	$smo.Databases['tempdb'].ExecuteNonQuery("DROP TABLE $tblname;")
+
+	return $output | Sort-Object -Property wait_time_ms -Descending | Select-Object -First 10
+
+
+}

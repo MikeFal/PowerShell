5b15981cf8ee513aab16f33e373c3751f2f92b33
Mike F <mike@mikefal.net>
2015-11-28 11:36:45 -0700

    Changed Set-SQlServiceAccount.ps1: -Altered function to take credential instead of plaintext login and password -Added check at end of function to start Agent service if it isn't running -Removed Write-Warning about restarting service (since WMI call will restart the service on its own) -Updated example to use credential object

--- Set-SqlServiceAccount.ps1
+++ Set-SqlServiceAccount.ps1
@@ -1,28 +1,40 @@
 ﻿#HT http://blogs.technet.com/b/heyscriptingguy/archive/2010/11/03/use-powershell-to-change-sql-server-s-service-accounts.aspx
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')| Out-Null
-
 function Set-SqlServiceAccount{
     param([string] $Instance
-        ,[string] $account
-        ,[string] $password
-        ,[ValidateSet('SqlServer','SqlAgent')] $service = 'SqlServer'
+        ,[System.Management.Automation.PSCredential]$ServiceAccount
+        ,[ValidateSet('SqlServer','SqlAgent')] $Service = 'SqlServer'
     )
     
-
+    #Get Service Account name from credential object
+    $account =$ServiceAccount.GetNetworkCredential().Domain +'\'+ $ServiceAccount.GetNetworkCredential().UserName
+    
+    #Parse host and instance names
     $HostName = ($instance.Split('\'))[0]
     $InstanceName = ($instance.Split('\'))[1]
 
+    #Get service account names, set service account for change
+    $sqlsvc = if($InstanceName){"MSSQL`$$InstanceName"}else{'MSSQLSERVER'}
+    $agtsvc = if($InstanceName){"SQLAGENT`$$InstanceName"}else{'SQLSERVERAGENT'}
+
     $ServiceName = switch($service){
-        'SqlServer'{if($InstanceName){"MSSQL`$$InstanceName"}else{'MSSQLSERVER'}}
-        'SqlAgent'{if($InstanceName){"SQLAGENT`$$InstanceName"}else{'SQLSERVERAGENT'}}
+        'SqlServer'{$sqlsvc}
+        'SqlAgent'{$agtsvc}
     }
 
+    #Use wmi to change account
     $smowmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $HostName
     $wmisvc = $smowmi.Services | Where-Object {$_.Name -eq $ServiceName}
+    $wmisvc.SetServiceAccount($account,$ServiceAccount.GetNetworkCredential().Password)
+    
+    #If Agent service isn't running (happens if you reset SQL Service), start it
+    $wmiagt = $smowmi.Services | Where-Object {$_.Name -eq $agtsvc}
+    if($wmiagt.ServiceSatus -ne 'Running'){$wmiagt.Start()}      
 
-    $wmisvc.SetServiceAccount($account,$password)
-    Write-Warning "Please restart $ServiceName on $HostName for changes to take effect."
 }
 
 
-Set-SqlServiceAccount -Instance PICARD -account 'SDF\sqlsvc2' -password 'P@$$w0rd' -service SqlServer
+<#
+$cred = Get-Credential 'Enter Service Account'
+Set-SqlServiceAccount -Instance PICARD -ServiceAccount $cred -Service SqlServer
+#>

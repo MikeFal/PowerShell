ef779874b5775afd98bf4397c5b93bce210a95ce
Mike Fal <mike@mikefal.net>
2018-01-03 11:26:09 -0700

    Rough restore example for SQL Cruise alumni.

---
+++ SQLCruise2017-RestoreBackups.ps1
@@ -0,0 +1,13 @@
+﻿#Get collection of files to loop through
+$db = Get-ChildItem '\\TARKIN\C$\Backups' -Recurse | Where-Object {$_.Extension -eq '.bak'}
+
+#Use foreach to loop through each file in the array
+foreach($d in $db){
+    #Run a 'RESTORE HEADERONLY...' to get info on the database to restore (mostly name)
+    $header = Invoke-Sqlcmd -ServerInstance TARKIN -Database tempdb "RESTORE HEADERONLY FROM DISK='$($d.FullName)'"
+
+    #Restore the database
+    Restore-SqlDatabase -ServerInstance TARKIN -Database $header.DatabaseName -BackupFile $d -Script
+}
+
+#Get-Help Restore-SqlDatabase
